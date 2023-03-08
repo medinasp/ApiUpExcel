b70572a9edc21732e245e6b6737a7723496cf2ca
unknown <medinasp@gmail.com>
2023-03-07 22:37:00 -0300

    Initial Commit

---
+++ ApiUpExcel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApiUpExcel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
